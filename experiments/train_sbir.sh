b9522933b1e615b4a90d3af5face1e0ebf0ae699
yuchuochuo1023 <qiany1023@gmail.com>
2016-05-22 11:56:21 +0100

    Update train_sbir.sh

--- experiments/train_sbir.sh
+++ experiments/train_sbir.sh
@@ -3,7 +3,7 @@
 
 python tools/sbir_train_net.py --gpu 0 \
 --solver ./experiments/solver.prototxt \
---output ./snapshot/sbir/ \      
+--output ./snapshot/ \      # make sure you create a folder named 'snapshot' under your $SBIR_ROOT folder
 --iters 25000 \
 --snapstep 500 \
 --weights ./experiments/init/sketchnet_init.caffemodel
