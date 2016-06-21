0430e470bb52fb88714328f58de10205235ab4cb
Jeffrey <sjfdlut@163.com>
2016-06-21 20:44:33 +0100

    Delete empty folder

--- download_data.sh
+++ download_data.sh
@@ -2,5 +2,7 @@
 
 wget "http://www.eecs.qmul.ac.uk/~qian/Qian's%20Materials/sbir_cvpr2016.zip"
 unzip -o sbir_cvpr2016.zip
-mv sbir_cvpr2016_release data
+mkdir data
+mv sbir_cvpr2016_release/* data/
+rm -r sbir_cvpr2016_release
 
