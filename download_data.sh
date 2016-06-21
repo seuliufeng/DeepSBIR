b51b66149f547c8fb8f5eb0f3ffe630b4955c958
Feng <liufeng@seu.edu.cn>
2016-06-21 20:30:33 +0100

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,5 @@
 
 wget "http://www.eecs.qmul.ac.uk/~qian/Qian's%20Materials/sbir_cvpr2016.zip"
 unzip -o sbir_cvpr2016.zip
-mkdir data
-mv sbir_cvpr2016_release/* data/
+mv sbir_cvpr2016_release data
 
