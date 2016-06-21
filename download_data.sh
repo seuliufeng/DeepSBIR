ea6051953d89661744fa0e4aba5ae582867d631e
Jeffrey <sjfdlut@163.com>
2016-06-21 22:00:18 +0100

    Add code to download model

--- download_data.sh
+++ download_data.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# download data
 wget "http://www.eecs.qmul.ac.uk/~qian/Qian's%20Materials/sbir_cvpr2016.zip"
 unzip -o sbir_cvpr2016.zip
-mkdir data
-mv sbir_cvpr2016_release/* data/
+rsync -a sbir_cvpr2016_release/ data/
 rm -r sbir_cvpr2016_release
+rm sbir_cvpr2016.zip
 
+# download model
+wget "http://www.eecs.qmul.ac.uk/~qian/Qian's%20Materials/sbir_models.zip"
+unzip -o sbir_models.zip
+rsync -a models/ data/models
+rm -r models
+rm sbir_models.zip
