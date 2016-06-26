8ccf076ef35f1428f8053ee792ec7dae86eb55eb
Feng <liufeng@seu.edu.cn>
2016-06-26 11:38:56 +0100

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -7,9 +7,12 @@ rsync -a sbir_cvpr2016_release/ data/
 rm -r sbir_cvpr2016_release
 rm sbir_cvpr2016.tar
 
+# remove features in data/feats to avoid feature conflicts caused by different models
+rm data/feats/*
+
 # download model
-wget "http://www.eecs.qmul.ac.uk/~qian/Qian's%20Materials/sbir_models.zip"
-unzip -o sbir_models.zip
+wget "http://www.eecs.qmul.ac.uk/%7Eqian/Qian%27s%20Materials/sbir_models.tar"
+tar -xvf sbir_models.tar
 rsync -a models/ data/models
 rm -r models
-rm sbir_models.zip
+rm sbir_models.tar
